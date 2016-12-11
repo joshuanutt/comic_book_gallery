765ee311e50e40b0ab25bc4836dcf163e306efb6
Jimmy Rustles <Jimmy Rustles@JIMMYRUSTLES-PC>
2016-12-11 11:22:30 -0600

    Updated comic book controller too use comic book repo

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,22 +10,22 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
         {
-            var comicBook = new ComicBook()
+            _comicBookRepository = new Data.ComicBookRepository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
             {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p> Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                            new Artist() { Role = "script", Name = "dan slott" },
-                            new Artist() { Role = "pencils", Name = "humberto ramos" },
-                            new Artist() { Role = "inks", Name = "victor olazaba" },
-                            new Artist() { Role = "colors", Name = "edgar delgado" },
-                            new Artist() { Role = "letters", Name = "chris eliopoulos" },
-                }
-            };
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+
             return View(comicBook);
         }
     }
